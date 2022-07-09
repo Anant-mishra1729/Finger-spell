6464293cec7a4deeae37560f3491d91f3a0028a5
Anant <amishra1577@gmail.com>
2022-07-09 05:52:50 +0530

    Modified Test_sign.py

--- Test_sign.py
+++ Test_sign.py
@@ -14,6 +14,7 @@ for file in files:
     with open(file, 'rb') as handle:
         filedict.append(pickle.load(handle))
 
+print(filedict)
 
 def eucledianDistances(handData, palmdist):
     distMatrix = np.zeros([len(handData), len(handData)], dtype='float')
@@ -50,27 +51,32 @@ def findError(known, test):
 
 # For testing using camera
 
+
 count = 0
 while True:
     success, image = cap.read()
-    image = cv2.flip(image, 1)
-    results = tracker.findHands(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    positions = []
-    palmdist = []
-    if results:
-        for landmarks in results:
-            # tracker.drawHands(image,landmarks)
-            position = tracker.getPos(landmarks.landmark, image.shape, points)
-            palmdist.append(((position[0][0]-position[3][0])**2 + (position[0][1]-position[3][1])**2)**0.5)
-            positions.extend(position)
-        test = eucledianDistances(positions, palmdist)
-        err_matrix = []
-        for gesture in filedict:
-            if len(gesture[0]) == len(test):
-                err_matrix.append((findError(gesture[0], test), gesture[1]))
-        if len(err_matrix):
-            cv2.putText(image, min(err_matrix)[1], (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-    Fps(image)
-    cv2.imshow("Output", image)
-    if cv2.waitKey(1) & 0xff == ord('q'):
+    if success:
+        image = cv2.flip(image, 1)
+        results = tracker.findHands(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        positions = []
+        palmdist = []
+        if results:
+            for landmarks in results:
+                # tracker.drawHands(image,landmarks)
+                position = tracker.getPos(landmarks.landmark, image.shape, points)
+                palmdist.append(((position[0][0]-position[3][0])**2 + (position[0][1]-position[3][1])**2)**0.5)
+                positions.extend(position)
+            test = eucledianDistances(positions, palmdist)
+            err_matrix = []
+            for gesture in filedict:
+                if len(gesture[0]) == len(test):
+                    err_matrix.append((findError(gesture[0], test), gesture[1]))
+            if len(err_matrix):
+                cv2.putText(image, min(err_matrix)[1], (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        Fps(image)
+        cv2.imshow("Output", image)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+    else:
+        print("Camera not found!")
         break
